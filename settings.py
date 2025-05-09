65e9dab84377191e80877afcfe19a7044a57c515
manasa794 <manasakmanasa30@gmail.com>
2025-05-09 11:46:29 +0530

    settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,6 @@
-
+pip install dj-database-url
+ import dj-database-url
+ import os
+ DATABASES = {
+ "default": dj_database_url.parse(os.environ.get("postgresql://test_db_koj7_user:u9x4Vk40rFihJvZi8i7qEDdEBl30GEiy@dpg-d0epek49c44c738a76v0-a.oregon-postgres.render.com/test_db_koj7"))
+}
